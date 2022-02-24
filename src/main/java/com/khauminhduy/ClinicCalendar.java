820c79f213e54e1643f0837d1aff8776b758b333
khauminhduy <khauminhduy@gmail.com>
2022-02-24 17:06:00 +0700

    update

--- src/main/java/com/khauminhduy/ClinicCalendar.java
+++ src/main/java/com/khauminhduy/ClinicCalendar.java
@@ -1,9 +1,11 @@
 package com.khauminhduy;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import lombok.Getter;
 
@@ -11,23 +13,43 @@ import lombok.Getter;
 public class ClinicCalendar {
 
 	private List<PatientAppointment> appointments;
+	private LocalDate today;
 
 	public ClinicCalendar() {
 		appointments = new ArrayList<>();
 	}
 
+	public ClinicCalendar(LocalDate today) {
+		this.today = today;
+		this.appointments = new ArrayList<>();
+	}
+
 	public void addAppointment(String patientFirstName, String patientLastName, String dockerKey, String dateTime) {
 		Doctor doctor = Doctor.valueOf(dockerKey);
+		LocalDateTime localDateTime = convertDateTimefromString(dateTime);
+		PatientAppointment patientAppointment = new PatientAppointment(patientFirstName, patientLastName, localDateTime,
+				doctor);
+		appointments.add(patientAppointment);
+	}
+
+	public List<PatientAppointment> getTodayAppointment() {
+		return appointments.stream().filter(appt -> appt.getAppointmentDateTime().toLocalDate().equals(today))
+				.collect(Collectors.toList());
+	}
+
+	public boolean hasAppointment(LocalDate date) {
+		return appointments.stream().anyMatch(appt -> appt.getAppointmentDateTime().toLocalDate().equals(date));
+	}
+
+	private LocalDateTime convertDateTimefromString(String dateTime) {
 		LocalDateTime localDateTime = null;
 		try {
 			localDateTime = LocalDateTime.parse(dateTime.toUpperCase(),
 					DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
 		} catch (Exception e) {
-			e.printStackTrace();
+			throw new RuntimeException(e);
 		}
-		PatientAppointment patientAppointment = new PatientAppointment(patientFirstName, patientLastName, localDateTime,
-				doctor);
-		appointments.add(patientAppointment);
+		return localDateTime;
 	}
 
 }
--- src/test/java/com/khauminhduy/TestClinicCalendar.java
+++ src/test/java/com/khauminhduy/TestClinicCalendar.java
@@ -1,21 +1,73 @@
 package com.khauminhduy;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.time.LocalDate;
 import java.util.List;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-public class TestClinicCalendar {
+class TestClinicCalendar {
 
 	@Test
-	public void allowEntryOfAppointment() {
+	void allowEntryOfAppointment() {
+		ClinicCalendar calendar = new ClinicCalendar();
+		calendar.addAppointment("Khau", "Duy", "avery", "2022-02-23 12:00:00");
+		List<PatientAppointment> appointments = calendar.getAppointments();
+		assertEquals(1, appointments.size());
+		PatientAppointment patientAppointment = appointments.get(0);
+		assertEquals("Khau", patientAppointment.getPatientFirstName());
+		assertEquals("Duy", patientAppointment.getPatientLastName());
+	}
+
+	@Test
+	void allowEntryOfAppointmentV2() {
 		ClinicCalendar calendar = new ClinicCalendar();
 		calendar.addAppointment("Khau", "Duy", "avery", "2022-02-23 12:00:00");
 		List<PatientAppointment> appointments = calendar.getAppointments();
 		assertNotNull(appointments);
 		assertEquals(1, appointments.size());
+		PatientAppointment patientAppointment = appointments.get(0);
+		assertAll(
+			() -> assertEquals("Khau", patientAppointment.getPatientFirstName()),
+			() -> assertEquals("Duy", patientAppointment.getPatientLastName())
+		);
+
 	}
-	
+
+	@Test
+	void returnTrueForHasAppointmentIfThereAreAppointment() {
+		ClinicCalendar calendar = new ClinicCalendar(LocalDate.now());
+		calendar.addAppointment("Khau", "Duy", "avery", "2022-02-24 00:00:00");
+		assertTrue(calendar.hasAppointment(LocalDate.of(2022, 02, 24)));
+	}
+
+	@Test
+	void returnFalseForHasAppointmentIfThereAreNoAppointment() {
+		ClinicCalendar calendar = new ClinicCalendar(LocalDate.now());
+		assertFalse(calendar.hasAppointment(LocalDate.now()));
+	}
+
+	@Test
+	void returnCurrentDayAppointment() {
+		ClinicCalendar calendar = new ClinicCalendar(LocalDate.now());
+		calendar.addAppointment("Khau", "Duy", "avery", "2022-02-24 14:00:00");
+		calendar.addAppointment("Khau2", "Duy2", "avery", "2022-02-24 15:00:00");
+		calendar.addAppointment("Khau3", "Duy3", "avery", "2022-02-25 14:00:00");
+		assertEquals(2, calendar.getTodayAppointment().size());
+		assertEquals(3, calendar.getAppointments().size());
+	}
+
+	@Test
+	void testThrowException() {
+		ClinicCalendar calendar = new ClinicCalendar(LocalDate.now());
+		assertThrows(Exception.class, () -> calendar.addAppointment("Khau", "Duy", "avery", "2022-02-24 12:00"));
+	}
+
+
 }
